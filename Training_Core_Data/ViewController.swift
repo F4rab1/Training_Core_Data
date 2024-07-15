f97cef912c8854175d378983c3ad2668685e905b
F4rab1 <issafarabi2022@gmail.com>
2024-07-15 23:25:23 +0500

    Initial Commit

---
+++ Training_Core_Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Training_Core_Data
+//
+//  Created by Фараби Иса on 15.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
