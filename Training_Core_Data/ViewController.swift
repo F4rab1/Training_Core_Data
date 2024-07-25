7e69a8ca1fe93dbf8febfcaa6477deeea24fcb4b
F4rab1 <issafarabi2022@gmail.com>
2024-07-25 23:35:27 +0500

    Some Change

--- Training_Core_Data/ViewController.swift
+++ Training_Core_Data/ViewController.swift
@@ -14,8 +14,26 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         
         navigationItem.title = "Companies"
-        navigationController?.navigationBar.prefersLargeTitles = true
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
+        
+        setupNavigationStyle()
+        
+    }
+    
+    @objc func handleAddCompany() {
+        print("Adding company..")
+    }
+    
+    func setupNavigationStyle() {
+        navigationController?.navigationBar.isTranslucent = false
+
+        let lightRed = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
+
+        navigationController?.navigationBar.backgroundColor = lightRed
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
     }
 
 
